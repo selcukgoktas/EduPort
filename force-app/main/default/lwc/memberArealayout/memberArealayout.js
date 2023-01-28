d2148b3c79f230c3182601bd523a9bf1cfc9601e
SELCUK GOKTAS <goktasselcuk1982@gmail.com>
2023-01-28 03:39:58 +0300

    member area %25 finished

--- force-app/main/default/lwc/memberArealayout/memberArealayout.js
+++ force-app/main/default/lwc/memberArealayout/memberArealayout.js
@@ -1,3 +1,122 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class MemberArealayout extends LightningElement {}
+export default class MemberArealayout extends NavigationMixin(LightningElement) {
+
+  canvasstyle='canvas';
+
+  guestId; // store on sessionStorage
+
+  courseId; // store on localStorage 
+
+  lessonId; // store on localStorage 
+
+
+  showMemberArea=false;
+
+  constructor(){
+    super();
+
+  }
+
+
+  connectedCallback(){
+
+    this.guestId= sessionStorage.getItem('Gid');
+
+    this.courseId= localStorage.getItem('Gid');
+
+    this.lessonId= localStorage.getItem('Gid');
+
+    if(!this.courseId){
+      // use it on html
+    } else {
+      // get the first course id
+    }
+
+    if(!this.lessonId){
+      // use it on html
+    } else {
+      // Use the first lesson id 
+    }
+
+    console.log(this.guestId);
+
+    if(!this.guestId){
+      this.navto('memberlogin__c');
+    } else{
+      this.showMemberArea=true;
+    }
+
+  }
+
+
+
+  navto(redirectPage) {
+    // Use the basePath from the Summer '20 module to construct the URL
+
+  console.log(`redirectPage   : ${redirectPage}`);
+
+
+  this[NavigationMixin.Navigate]({
+    type: 'comm__namedPage',
+    attributes: {
+        name: 'memberlogin__c'
+    }
+    });
+
+
+
+
+  }
+
+
+
+
+  openNav(){
+
+    /**
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+     */
+
+    const sidebarshow = this.template.querySelector('[data-id="sidebar"]');
+
+    const canvasshow=this.template.querySelector('[data-id="canvas"]');
+
+
+ // marginLeft is not working 
+    canvasshow.marginLeft='250px';
+    sidebarshow.style.width='250px';
+
+    this.canvasstyle='canvas-right';
+
+    
+  }
+
+
+  closeNav(){
+
+    /**
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+
+
+
+     */
+
+
+      const sidebarhide = this.template.querySelector('[data-id="sidebar"]');
+
+      const canvashide=this.template.querySelector('[data-id="canvas"]');
+
+   
+      // marginLeft is not working 
+
+      canvashide.marginLeft='0';
+    sidebarhide.style.width='0';
+
+    this.canvasstyle='canvas';
+
+  }
+}
